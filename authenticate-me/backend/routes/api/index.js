f07e5aabab82f789a54119520f46889ee8299807
JohnCarrera <johnrcarrera@gmail.com>
2022-08-26 17:27:29 -0400

    user login added and tested locally

--- authenticate-me/backend/routes/api/index.js
+++ authenticate-me/backend/routes/api/index.js
@@ -1,5 +1,7 @@
 // backend/routes/api/index.js
 const router = require('express').Router();
+const sessionRouter = require('./session.js');
+const usersRouter = require('./users.js');
 const { setTokenCookie } = require('../../utils/auth.js');
 const { User } = require('../../db/models');
 const { restoreUser } = require('../../utils/auth.js');
@@ -9,6 +11,13 @@ const { requireAuth } = require('../../utils/auth.js');
   // If current user session is not valid, set req.user to null
 router.use(restoreUser);
 
+router.use('/session', sessionRouter);
+
+router.use('/users', usersRouter);
+
+router.post('/test', (req, res) => {
+  res.json({ requestBody: req.body });
+});
 // router.get('/restore-user', (req, res) => {
 //     return res.json(req.user);
 //   }
