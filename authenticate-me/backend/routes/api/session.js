a4735e4aed99a7c0e72f985a7657aeb057c06067
JohnCarrera <johnrcarrera@gmail.com>
2022-08-26 17:30:51 -0400

    user logout added and tested locally

--- authenticate-me/backend/routes/api/session.js
+++ authenticate-me/backend/routes/api/session.js
@@ -5,6 +5,12 @@ const { User } = require('../../db/models');
 
 const router = express.Router();
 
+router.delete('/', (_req, res) => {
+    res.clearCookie('token');
+    return res.json({ message: 'success' });
+  }
+);
+
 router.post('/', async (req, res, next) => {
     const { credential, password } = req.body;
 
