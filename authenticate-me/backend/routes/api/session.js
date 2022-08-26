a26917e7620e59315e325b6231abebfdcb969b40
JohnCarrera <johnrcarrera@gmail.com>
2022-08-26 18:28:45 -0400

    get-session implemented and working locally

--- authenticate-me/backend/routes/api/session.js
+++ authenticate-me/backend/routes/api/session.js
@@ -11,6 +11,16 @@ router.delete('/', (_req, res) => {
   }
 );
 
+router.get('/', restoreUser, (req, res) => {
+    const { user } = req;
+    if (user) {
+      return res.json({
+        user: user.toSafeObject()
+      });
+    } else return res.json({});
+  }
+);
+
 router.post('/', async (req, res, next) => {
     const { credential, password } = req.body;
 
