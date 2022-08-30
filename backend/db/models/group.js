80723bac9a17bf935f46b51862c966ad9ffb2e4f
JohnCarrera <johnrcarrera@gmail.com>
2022-08-30 16:49:14 -0400

    added associations for group model

--- backend/db/models/group.js
+++ backend/db/models/group.js
@@ -1,7 +1,6 @@
 'use strict';
-const {
-  Model
-} = require('sequelize');
+const { Model} = require('sequelize');
+
 module.exports = (sequelize, DataTypes) => {
   class Group extends Model {
     /**
@@ -10,7 +9,26 @@ module.exports = (sequelize, DataTypes) => {
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
-      // define association here
+      Group.belongsToMany(models.User, {
+          through: models.Membership
+        });
+
+      Group.belongsTo(models.User,{
+          foreignKey: 'organizerId',
+          allowNull: false
+        });
+
+      Group.hasMany(models.Venue, {
+          foreignKey: 'groupId'
+        });
+
+      Group.hasMany(models.GroupImage, {
+        foreignKey: 'groupId'
+      });
+
+      Group.hasMany(models.Event, {
+        foreignKey: 'groupId'
+      })
     }
   }
   Group.init({
