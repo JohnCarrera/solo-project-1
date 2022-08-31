40bd2035c1e2207e545fe3608a4316d5ebba23f9
JohnCarrera <johnrcarrera@gmail.com>
2022-08-31 01:34:21 -0400

    added seeder for eventimages and memberships

--- backend/db/seeders/20220831004123-test-memberships.js
+++ backend/db/seeders/20220831004123-test-memberships.js
@@ -2,23 +2,127 @@
 
 module.exports = {
   async up (queryInterface, Sequelize) {
-    /**
-     * Add seed commands here.
-     *
-     * Example:
-     * await queryInterface.bulkInsert('People', [{
-     *   name: 'John Doe',
-     *   isBetaMember: false
-     * }], {});
-    */
+    return queryInterface.bulkInsert('GroupImages', [
+      {
+        id: 1,
+        userId: 1,
+        groupId: 1,
+        status: 'current'
+      },
+      {
+        id: 2,
+        userId: 2,
+        groupId: 1,
+        status: 'current'
+      },
+      {
+        id: 3,
+        userId: 3,
+        groupId: 1,
+        status: 'current'
+      },
+      {
+        id: 4,
+        userId: 6,
+        groupId: 1,
+        status: 'current'
+      },
+      {
+        id: 5,
+        userId: 4,
+        groupId: 2,
+        status: 'current'
+      },
+      {
+        id: 6,
+        userId: 5,
+        groupId: 2,
+        status: 'current'
+      },
+      {
+        id: 7,
+        userId: 7,
+        groupId: 2,
+        status: 'current'
+      },
+      {
+        id: 8,
+        userId: 9,
+        groupId: 2,
+        status: 'current'
+      },
+      {
+        id: 9,
+        userId: 3,
+        groupId: 3,
+        status: 'current'
+      },
+      {
+        id: 10,
+        userId: 10,
+        groupId: 3,
+        status: 'current'
+      },
+      {
+        id: 11,
+        userId: 7,
+        groupId: 4,
+        status: 'current'
+      },
+      {
+        id: 12,
+        userId: 9,
+        groupId: 4,
+        status: 'current'
+      },
+      {
+        id: 13,
+        userId: 10,
+        groupId: 4,
+        status: 'current'
+      },
+      {
+        id: 14,
+        userId: 10,
+        groupId: 6,
+        status: 'current'
+      },
+      {
+        id: 15,
+        userId: 10,
+        groupId: 8,
+        status: 'current'
+      },
+      {
+        id: 16,
+        userId: 7,
+        groupId: 9,
+        status: 'current'
+      },
+      {
+        id: 17,
+        userId: 2,
+        groupId: 5,
+        status: 'current'
+      },
+      {
+        id: 18,
+        userId: 3,
+        groupId: 5,
+        status: 'current'
+      },
+      {
+        id: 19,
+        userId: 1,
+        groupId: 5,
+        status: 'current'
+      }
+    ], {});
   },
 
   async down (queryInterface, Sequelize) {
-    /**
-     * Add commands to revert seed here.
-     *
-     * Example:
-     * await queryInterface.bulkDelete('People', null, {});
-     */
+    return queryInterface.bulkDelete('Groups', {
+      id: { [Op.between]: [0,20]}
+    }, {});
   }
 };
--- backend/db/seeders/20220831004144-test-event-images.js
+++ backend/db/seeders/20220831004144-test-event-images.js
@@ -2,23 +2,73 @@
 
 module.exports = {
   async up (queryInterface, Sequelize) {
-    /**
-     * Add seed commands here.
-     *
-     * Example:
-     * await queryInterface.bulkInsert('People', [{
-     *   name: 'John Doe',
-     *   isBetaMember: false
-     * }], {});
-    */
+    return queryInterface.bulkInsert('GroupImages', [
+      {
+        id: 1,
+        eventId: 1,
+        url: 'https://imagehost.com/eventimage1',
+        preview: true
+      },
+      {
+        id: 2,
+        eventId: 2,
+        url: 'https://imagehost.com/eventimage2',
+        preview: true
+      },
+      {
+        id: 3,
+        eventId: 3,
+        url: 'https://imagehost.com/eventimage3',
+        preview: true
+      },
+      {
+        id: 4,
+        eventId: 4,
+        url: 'https://imagehost.com/eventimage4',
+        preview: true
+      },
+      {
+        id: 5,
+        eventId: 5,
+        url: 'https://imagehost.com/eventimage5',
+        preview: true
+      },
+      {
+        id: 6,
+        eventId: 1,
+        url: 'https://imagehost.com/eventimage11',
+        preview: true
+      },
+      {
+        id: 7,
+        eventId: 2,
+        url: 'https://imagehost.com/eventimage22',
+        preview: true
+      },
+      {
+        id: 8,
+        eventId: 3,
+        url: 'https://imagehost.com/eventimage33',
+        preview: true
+      },
+      {
+        id: 9,
+        eventId: 4,
+        url: 'https://imagehost.com/eventimage44',
+        preview: true
+      },
+      {
+        id: 10,
+        eventId: 5,
+        url: 'https://imagehost.com/eventimage55',
+        preview: true
+      }
+    ], {});
   },
 
   async down (queryInterface, Sequelize) {
-    /**
-     * Add commands to revert seed here.
-     *
-     * Example:
-     * await queryInterface.bulkDelete('People', null, {});
-     */
+    return queryInterface.bulkDelete('Groups', {
+      id: { [Op.between]: [0,11]}
+    }, {});
   }
 };
