758248b83c2ebb31141c1150bd2071ed4afb88a3
JohnCarrera <johnrcarrera@gmail.com>
2022-08-31 00:43:28 -0400

    added seeder file for groupImages

--- backend/db/seeders/20220831004112-test-group-images.js
+++ backend/db/seeders/20220831004112-test-group-images.js
@@ -2,15 +2,68 @@
 
 module.exports = {
   async up (queryInterface, Sequelize) {
-    /**
-     * Add seed commands here.
-     *
-     * Example:
-     * await queryInterface.bulkInsert('People', [{
-     *   name: 'John Doe',
-     *   isBetaMember: false
-     * }], {});
-    */
+    return queryInterface.bulkInsert('GroupImages', [
+      {
+        id: 1,
+        groupId: 1,
+        url: 'https://imagehost.com/groupimage1',
+        preview: true
+      },
+      {
+        id: 2,
+        groupId: 2,
+        url: 'https://imagehost.com/groupimage2',
+        preview: true
+      },
+      {
+        id: 3,
+        groupId: 3,
+        url: 'https://imagehost.com/groupimage3',
+        preview: true
+      },
+      {
+        id: 4,
+        groupId: 4,
+        url: 'https://imagehost.com/groupimage4',
+        preview: true
+      },
+      {
+        id: 5,
+        groupId: 5,
+        url: 'https://imagehost.com/groupimage5',
+        preview: true
+      },
+      {
+        id: 6,
+        groupId: 1,
+        url: 'https://imagehost.com/groupimage11',
+        preview: true
+      },
+      {
+        id: 7,
+        groupId: 2,
+        url: 'https://imagehost.com/groupimage22',
+        preview: true
+      },
+      {
+        id: 8,
+        groupId: 3,
+        url: 'https://imagehost.com/groupimage33',
+        preview: true
+      },
+      {
+        id: 9,
+        groupId: 4,
+        url: 'https://imagehost.com/groupimage44',
+        preview: true
+      },
+      {
+        id: 10,
+        groupId: 5,
+        url: 'https://imagehost.com/groupimage55',
+        preview: true
+      }
+    ], {});
   },
 
   async down (queryInterface, Sequelize) {
