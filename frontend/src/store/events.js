313be5372dc3896b77dfdf50e609e98756855612
JohnCarrera <johnrcarrera@gmail.com>
2022-09-20 16:11:54 -0400

    completed first draft of event store file with actions, thunks, reducers

--- frontend/src/store/events.js
+++ frontend/src/store/events.js
@@ -31,6 +31,75 @@ export const getAllEvents = () => async dispatch => {
 
     if(res.ok) {
         const events = await res.json();
-        console.log('group')
+        console.log('events:', events);
+        dispatch(loadAll(events));
+        return events;
+    }
+    return null;
+}
+
+export const getSingleEvent = (id) => async dispatch => {
+    const res = await fetch(`/api/events/${id}`);;
+
+    if(res.ok) {
+        const event = await res.json();
+        console.log('event:', event);
+        dispatch(loadOne(event));
+        return event;
+    }
+    return null;
+}
+
+export const createEvent = (event) => async dispatch => {
+    const res = await fetch('/api/events', {
+        method: 'POST',
+        body: JSON.stringify(event)
+    });
+
+    if(res.ok) {
+        const event = await res.json();
+        console.log('eventcreate:', event);
+        dispatch(loadAll(event));
+        return event;
+    }
+    return null;
+}
+
+export const deleteEvent = (id) => async dispatch => {
+    const res = await fetch(`/api/events/${id}`);
+
+    if(res.ok) {
+        const delRes = await res.json();
+        console.log('delEvent in Thunk: ', delRes);
+        dispatch(del());
+    }
+    return null;
+}
+
+const initialState = {
+    allEvents: {},
+    singleEvent: {
+        Group: {}
+    }
+}
+
+export const eventReducer = (state = initialState, action) => {
+    switch (action.type) {
+        case LOAD_EVENTS:
+            const allEvents = {};
+            action.eventList.reduce((acc, cur) => {
+                acc[cur.id] = cur;
+                return acc;
+            }, allEvents);
+            return { ...state, allEvents: allEvents }
+
+        case LOAD_ONE_EVENT:
+        case ADD_EVENT:
+            const singleEvent = action.group;
+            return { ...state, singleEvent: singleEvent }
+
+        case DELETE_EVENT:
+            const delState = {...state, singleEvent: {}};
+            return delState;
     }
 }
