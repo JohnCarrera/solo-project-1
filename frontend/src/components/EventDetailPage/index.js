3e460378a7350fb2c97319f327046bccee96bea9
JohnCarrera <johnrcarrera@gmail.com>
2022-09-23 18:41:28 -0400

    basic div frame in place for page layout

--- frontend/src/components/EventDetailPage/index.js
+++ frontend/src/components/EventDetailPage/index.js
@@ -19,12 +19,29 @@ export default function EventDetailPage() {
     const dispatch = useDispatch();
     const history = useHistory();
 
+    const [daysOfWeek, setDaysOfWeek] = useState([
+        'SUN', 'MON', 'TUE', 'WED', 'THU', 'FRI', 'SAT'
+    ]);
+    const [months, setMonths] = useState([
+        'JAN', 'FEB', 'MAR', 'APR', 'MAY', 'JUN',
+        'JUL', 'AUG', 'SEP', 'OCT', 'NOV', 'DEC'
+    ]);
+    const [eventDate, setEventDate] = useState(
+        new Date((event.startDate.substring(0, event.startDate.length - 5)))
+    );
+    const [eventDay, setEventDay] = useState(daysOfWeek[eventDate.getDay()]);
+    const [eventMonth, setEventMonth] = useState(months[eventDate.getMonth()]);
+    const [eventDayOfMonth, setEventDayOfMonth] = useState(eventDate.getDate());
+    const [eventYear, setEventYear] = useState(eventDate.getFullYear());
+    const [eventHours, setEventHours] = useState(eventDate.getHours());
+    const [eventMinutes, setEventMinutes] = useState(eventDate.getMinutes());
+
     useEffect(() => {
         dispatch(getSingleEvent(eventId))
     }, [dispatch]);
 
     useEffect(() => {
-        if(event.Group){
+        if (event.Group) {
             dispatch(getSingleGroup(event.Group.id))
         }
     }, [event]);
@@ -38,20 +55,56 @@ export default function EventDetailPage() {
 
 
     return (
-        <div>
-            <div>EventDetailPage</div>
-            <div>{event.name}</div>
-            <div>Details: {event.description}</div>
+        <div className='ed-main-page-body'>
+            <div className='ed-top-info-banner'>
+                <div className='ed-info-banner-date'>
+
+                </div>
+                <div className='ed-info-banner-title'>
+                    {event.name}
+                </div>
+                <div className='ed-info-banner-host'>
+
+                </div>
+            </div>
+            <div className='ed-page-body'>
+                <div className='ed-left-side-grp'>
+                    <img
+                        className='ed-main-img'
+                        src='https://images.pexels.com/photos/36039/baby-twins-brother-and-sister-one-hundred-days.jpg'
+                    />
+                    <div>Details</div>
+                    <div>
+                        {event.description}
+                    </div>
+                </div>
+                <div className='ed-rt-side-grp'>
+                    <div className='ed-group-detail-item'>
+                        <img
+                            className='ed-group-item-img'
+                            src='https://images.pexels.com/photos/36039/baby-twins-brother-and-sister-one-hundred-days.jpg'
+                        />
+                        <div className='ed-group-detail-text'>
+                            <div className='ed-group-detail-title'>
+
+                            </div>
+                            <div className='ed-group-detail-public'>
+
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
             <div>Start: {event.startDate}</div>
             <div>End: {event.endDate}</div>
             <div>{event.numAttending} Attending</div>
             <div>Image {event.previewImage}</div>
 
             {event.Group &&
-            <>
-            <div>Group {event.Group.name}</div>
-            <div>Location {event.Group.city + ', ' + event.Group.state}</div>
-            </>
+                <>
+                    <div>Group {event.Group.name}</div>
+                    <div>Location {event.Group.city + ', ' + event.Group.state}</div>
+                </>
             }
             {user && group && user.id === group.organizerId &&
                 <div className='ed-owner-buttons'>
